2119c52a5a3834127efe85905fdba2db0f8759a7
pratikm121 <pratikm121@gmail.com>
2018-05-01 17:55:14 +0530

    Commited to Head Directly

--- TestJavaProject/src/com/pratik/git/test/MyFirstGitTest.java
+++ TestJavaProject/src/com/pratik/git/test/MyFirstGitTest.java
@@ -3,8 +3,7 @@ package com.pratik.git.test;
 public class MyFirstGitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hi there I made my first change!!!!!");
-
+		System.out.println("Hi there I made my first change in the head!!!!!");
 	}
 
 }
